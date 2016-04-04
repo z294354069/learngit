ea11e6d229270ec87eb3ccf0ac2e90851e6d2fcf
zrjy <zrjytju@yeah.net>
2016-04-04 21:14:06 +0800

    添加了note下载和上传

--- evernote/EvernoteControl.py
+++ evernote/EvernoteControl.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
-# -*- coding: iso-8859-15 -*-
+# -*- coding: utf-8 -*-
 import os, sys
 
 from evernote.api.client import EvernoteClient
+import evernote.edam.notestore.NoteStore as NoteStore
 import evernote.edam.type.ttypes as Types
 import evernote.edam.error.ttypes as Error
 
 SERVICE_HOST = 'app.yinxiang.com'
-Dev_Token = ''
+Dev_Token = 'S=s16:U=33fa90:E=15b37ecfe6b:C=153e03bd190:P=1cd:A=en-devtoken:V=2:H=9cecf8305cc371bb7a2c1ce1ec6292cb'
 
 
 class EvernoteControl:
@@ -94,14 +95,43 @@ class EvernoteControl:
 
         return note
 
+    def showNote(self):
+        filter = NoteStore.NoteFilter()
+        filter.ascending = False
+        spec = NoteStore.NotesMetadataResultSpec()
+        spec.includeTitle = True
+        ourNoteList = self.noteStore.findNotesMetadata(Dev_Token, filter, 0, 100, spec)
+
+        wholeNotes = []
+        for note in ourNoteList.notes:
+            # wholeNote = self.noteStore.getNote(Dev_Token, note.guid, True, False, False, False)
+            # print("Content length: %d" % wholeNote.contentLength)
+            # wholeNotes.append(wholeNote)
+            # for note in wholeNotes:
+            #     print(note.content)
+            print(self.noteStore.getNoteContent(Dev_Token, note.guid))
 
 
 
 if __name__ == '__main__':
     evernote = EvernoteControl()
-    evernote.makeNotebook('张家语zrjy')
-    # evernote.displayNoteBook()
+    # evernote.makeNotebook('张家语zrjy')
+    # # evernote.displayNoteBook()
     # evernote.deleteNotebook("python notebook")
-    print("\r\n")
-    evernote.displayNoteBook()
-    evernote.makeNote('python note', 'this note is from python张忍', "python notebook")
+    # print("\r\n")
+    # evernote.displayNoteBook()
+    # evernote.makeNote('python note', 'this note is from python张忍', "python notebook")
+    # evernote.showNote()
+
+
+
+    path = "C:\Users\Administrator\Desktop\dev_token.txt"
+    with open(path, "r") as f:
+        content = f.read()
+    fileName = path.split("\\")
+    fileName = fileName.pop()
+    title = fileName.split(".")[0]
+    evernote.makeNote(title, content, "python notebook")
+
+    evernote.showNote()
+
